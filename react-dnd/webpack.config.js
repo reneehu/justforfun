853fbb7cad99d32c57e1c6d036188e9a5a1ec06a
reneehu <reneehu217@gmail.com>
2017-11-08 17:07:31 -0500

    ughhh drag and drop

---
+++ react-dnd/webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+    ],
+    loaders: [
+      {
+        exclude: /(node_modules|bower_components)/,
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+};
